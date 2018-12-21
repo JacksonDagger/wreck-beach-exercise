deddcc05e43a0c6976e70daca5e721ad00063075
JacksonDagger <JacksonDDagger@gmail.com>
2018-12-21 13:22:13 -0500

    Continued work

--- wreckBeach.py
+++ wreckBeach.py
@@ -1,1 +1,20 @@
+def wreckMain(stair, stamina):
+    return wreckRecursive(stair, stamina, -1)
+
+
+def wreckRecursive(stair, stamina, index):
+    length = len(stair)
+    if index >= length:
+        return stamina
+
+    maxStamina = wreckRecursive(stair, stamina-stair[index+1], index+1)
+    if index+1 < length:
+        tempStamina = wreckRecursive(stair, stamina-stair[index+2]-1, index+2)
+        if tempStamina > maxStamina:
+            maxStamina = tempStamina
+    if index+2 < length:
+        tempStamina = wreckRecursive(stair, stamina-stair[index+3]-2, index+3)
+        if tempStamina > maxStamina:
+            maxStamina = tempStamina
+    return maxStamina
 
---
+++ wreckTest.py
@@ -0,0 +1,5 @@
+import unittest
+import wreckBeach
+
+class TestWreck(unittest.TestCase):
+
